f618fe37d71d46b9561804d6e739371aa8469468
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-10-17 11:20:52 +0200

    fix: delete unnecessary code

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -6,10 +6,6 @@ const sumAll = function (num1, num2) {
     num2 > 0
   ) {
     let sum = 0;
-    if (num1 > num2) {
-      let start = num1;
-      let end = num2;
-    }
 
     if (num1 < num2) {
       for (let i = num1; i <= num2; i++) {
